c97d11e1d474dd209e5e4642ff14dc56c79812a4
Frey, David | Nemo <david.a.frey@rakuten.com>
2017-12-26 15:29:02 +0900

    reformat code and add jitpack to project repositories closure

--- buildSrc/src/main/groovy/com/rakuten/tech/tool/CheckGradleFilesForSnapshotDependencies.groovy
+++ buildSrc/src/main/groovy/com/rakuten/tech/tool/CheckGradleFilesForSnapshotDependencies.groovy
@@ -11,82 +11,83 @@ import static com.rakuten.tech.tool.Functions.findAllSnapshots
 
 @SuppressWarnings("GroovyUnusedDeclaration")
 public class CheckGradleFilesForSnapshotDependencies extends DefaultTask {
-    /**
-     * Regex or collection of Regexes to exclude files/paths from filtering.
-     * Example configurations:
-     *
-     * ```groovy
-     * // single
-     * exclude = ~/.*\/Example\/.*\.gradle/
-     *
-     * // list of Regexes
-     * exclude = [
-     *          ~/.*\/Example\/.*\.gradle/ ,
-     *          ~/.*\/Runtime\/.*\.gradle/
-     *          ]
-     * ```
-     */
-    def exclude
+  /**
+   * Regex or collection of Regexes to exclude files/paths from filtering.
+   * Example configurations:
+   *
+   * ```groovy
+   * // single
+   * exclude = ~/.*\/Example\/.*\.gradle/
+   *
+   * // list of Regexes
+   * exclude = [
+   *          ~/.*\/Example\/.*\.gradle/ ,
+   *          ~/.*\/Runtime\/.*\.gradle/
+   *          ]
+   * ```
+   */
+  def exclude
 
-    CheckGradleFilesForSnapshotDependencies() {
-        description = "Check build files for SNAPSHOT dependencies"
-        group = 'Verification'
+  CheckGradleFilesForSnapshotDependencies() {
+    description = "Check build files for SNAPSHOT dependencies"
+    group = 'Verification'
+  }
+
+  @TaskAction
+  void findAndCheckBuildFiles() {
+
+    println "Checking gradle build files for "
+
+    def visitGradleFile = {
+      print "checking $it... "
+      def script = new GradleBuildScript(it)
+      if (script.compileDependencies) {
+        def snapshots = findAllSnapshots(script.compileDependencies)
+        logger.debug("Found compile dependencies: $script.compileDependencies with snapshots: $snapshots")
+        if (snapshots) {
+          println "✗"
+          throw new GradleException(
+              "Found SNAPSHOT in compile dependencies in build script $it on $snapshots")
+        }
+      }
+
+      if (script.buildScriptDependencies) {
+        def snapshots = findAllSnapshots(script.buildScriptDependencies)
+        logger.debug("Found buildscript dependencies: $script.buildScriptDependencies with snapshots: $snapshots")
+        if (snapshots) {
+          println "✗"
+          throw new GradleException(
+              "Found SNAPSHOT in buildscript dependencies in build script $it on $snapshots")
+        }
+      }
+      println "✓"
     }
 
-    @TaskAction
-    void findAndCheckBuildFiles() {
+    def filter = { false }
+    def isGradleFile = { it ==~ /.*\.gradle$/ }
 
-        println "Checking gradle build files for "
-
-        def visitGradleFile = {
-            print "checking $it... "
-            def script = new GradleBuildScript(it)
-            if(script.compileDependencies) {
-                def snapshots = findAllSnapshots(script.compileDependencies)
-                logger.debug("Found compile dependencies: $script.compileDependencies with snapshots: $snapshots")
-                if(snapshots) {
-                    println "✗"
-                    throw new GradleException(
-                            "Found SNAPSHOT in compile dependencies in build script $it on $snapshots")
-                }
-            }
-
-            if(script.buildScriptDependencies) {
-                def snapshots = findAllSnapshots(script.buildScriptDependencies)
-                logger.debug("Found buildscript dependencies: $script.buildScriptDependencies with snapshots: $snapshots")
-                if(snapshots) {
-                    println "✗"
-                    throw new GradleException(
-                            "Found SNAPSHOT in buildscript dependencies in build script $it on $snapshots")
-                }
-            }
-            println "✓"
+    if (exclude instanceof Pattern) {
+      filter = exclude
+    } else if (exclude && [Collection, Object[]].any {
+      it.isAssignableFrom(exclude.getClass())
+    }) {
+      filter = { file ->
+        if (!isGradleFile(file.absolutePath)) return true
+        def shouldExclude = exclude.any {
+          file.absolutePath ==~ it
         }
-
-        def filter = { false }
-        def isGradleFile = { it ==~ /.*\.gradle$/ }
-
-        if (exclude instanceof Pattern) {
-            filter = exclude
-        } else if ( exclude && [Collection, Object[]].any { it.isAssignableFrom(exclude.getClass())
-        }) {
-            filter = { file ->
-                if(!isGradleFile(file.absolutePath)) return true
-                def shouldExclude = exclude.any {
-                    file.absolutePath ==~ it
-                }
-                if(shouldExclude) println "excluding file $file.absolutePath"
-                shouldExclude
-            }
-        }
-
-        project.projectDir.traverse type: FileType.FILES,
-                visit: visitGradleFile,
-                nameFilter: isGradleFile,
-                excludeFilter: filter
-
-        println "No SNAPSHOTS found ✓"
+        if (shouldExclude) println "excluding file $file.absolutePath"
+        shouldExclude
+      }
     }
+
+    project.projectDir.traverse type: FileType.FILES,
+        visit: visitGradleFile,
+        nameFilter: isGradleFile,
+        excludeFilter: filter
+
+    println "No SNAPSHOTS found ✓"
+  }
 
 }
 
--- buildSrc/src/test/groovy/com/rakuten/tech/tool/GradleBuildScriptSpec.groovy
+++ buildSrc/src/test/groovy/com/rakuten/tech/tool/GradleBuildScriptSpec.groovy
@@ -1,41 +1,42 @@
 package com.rakuten.tech.tool
 
 import org.junit.Test
-import static Functions.*
+
+import static com.rakuten.tech.tool.Functions.findAllSnapshots
 
 class GradleBuildScriptSpec {
 
-    @Test def void shouldFindCompileDependencies() {
-        def s = gradleScript("compile_dependency.gradle")
-        assert s.compileDependencies.size() == 2
-        assert s.buildScriptDependencies.empty
-    }
+  @Test def void shouldFindCompileDependencies() {
+    def s = gradleScript("compile_dependency.gradle")
+    assert s.compileDependencies.size() == 2
+    assert s.buildScriptDependencies.empty
+  }
 
-    @Test def void shouldFindBuildScriptDependencies() {
-        def s = gradleScript("build_script_dependency.gradle")
-        assert s.compileDependencies.empty
-        assert s.buildScriptDependencies.size() == 1
-    }
+  @Test def void shouldFindBuildScriptDependencies() {
+    def s = gradleScript("build_script_dependency.gradle")
+    assert s.compileDependencies.empty
+    assert s.buildScriptDependencies.size() == 1
+  }
 
-    @Test def void shouldNotFailWithNoDependencies() {
-        def s = gradleScript("no_dependency.gradle")
-        assert s.compileDependencies.empty
-        assert s.buildScriptDependencies.empty
-    }
+  @Test def void shouldNotFailWithNoDependencies() {
+    def s = gradleScript("no_dependency.gradle")
+    assert s.compileDependencies.empty
+    assert s.buildScriptDependencies.empty
+  }
 
-    @Test def void shouldFindSnapshots() {
-        def s = gradleScript("compile_dependency.gradle")
-        def snapshots = findAllSnapshots(s.compileDependencies)
-        assert snapshots.size() == 1
-    }
+  @Test def void shouldFindSnapshots() {
+    def s = gradleScript("compile_dependency.gradle")
+    def snapshots = findAllSnapshots(s.compileDependencies)
+    assert snapshots.size() == 1
+  }
 
-    static def gradleScript(name) {
-        def filePath = GradleBuildScriptSpec.getClassLoader().getResource(name)
-        println filePath
-        println filePath.path
-        def f = new File(filePath.path);
-        assert f
-        println f.text
-        new GradleBuildScript(f)
-    }
+  static def gradleScript(name) {
+    def filePath = GradleBuildScriptSpec.getClassLoader().getResource(name)
+    println filePath
+    println filePath.path
+    def f = new File(filePath.path);
+    assert f
+    println f.text
+    new GradleBuildScript(f)
+  }
 }
